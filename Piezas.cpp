85762d249ad564c5d5738f4569f7c3057c94b425
MisterC500 <misterc500@gmail.com>
2020-12-11 21:55:53 -0800

    Added to functionality, and implemented tests

--- Piezas.cpp
+++ Piezas.cpp
@@ -1,5 +1,6 @@
 #include "Piezas.h"
 #include <vector>
+#include <iostream>
 /** CLASS Piezas
  * Class for representing a Piezas vertical board, which is roughly based
  * on the game "Connect Four" where pieces are placed in a column and 
@@ -20,8 +21,11 @@
  * Constructor sets an empty board (default 3 rows, 4 columns) and 
  * specifies it is X's turn first
 **/
+
 Piezas::Piezas()
 {
+  board = std::vector<std::vector<Piece>>(BOARD_ROWS, std::vector<Piece>(BOARD_COLS,Blank));
+  turn = X;
 }
 
 /**
@@ -30,6 +34,11 @@ Piezas::Piezas()
 **/
 void Piezas::reset()
 {
+  for(int i = 0; i < BOARD_ROWS; i++)
+  {
+    for(int j = 0; j < BOARD_COLS; j++)
+      board[i][j] = Blank;
+  }
 }
 
 /**
@@ -42,6 +51,56 @@ void Piezas::reset()
 **/ 
 Piece Piezas::dropPiece(int column)
 {
+  //First check if given column is out of bounds
+  if(column >= BOARD_COLS)
+  {
+    //change turns if placed out of bounds
+    if(turn == X)
+      turn = O;
+    else
+      turn = X;
+    return Invalid; 
+  }
+
+  //Switch on turns X and O
+  switch(turn)
+  {
+    //if turn is X
+    case X:
+      {
+        for(int i = 0; i < BOARD_ROWS; i++)
+        {
+          for(int j = 0; j < BOARD_COLS; j++)
+          {
+            //check to see we have right column, and is not already taken up by X or O
+            if( (j == column) && (board[i][j] != X) && (board[i][j] != O) )
+            {
+              board[i][j] = X;
+              turn = O;
+              return X;
+            }
+          }
+        }//for
+        break;
+      }
+    //if turn is O
+    case O:
+      {
+        for(int i = 0; i < BOARD_ROWS; i++)
+        {
+          for(int j = 0; j < BOARD_COLS; j++)
+          {
+            if( j == column && (board[i][j] != X) && (board[i][j] != O) )
+            {
+              board[i][j] = O;
+              turn = X;
+              return O;
+            }
+          }
+        }//for
+        break;
+      }
+  }//switch
     return Blank;
 }
 
@@ -51,6 +110,18 @@ Piece Piezas::dropPiece(int column)
 **/
 Piece Piezas::pieceAt(int row, int column)
 {
+  //if row or column is out of bounds
+  if(row >= BOARD_ROWS || column >= BOARD_COLS)
+    return Invalid;
+
+  //create temp piece and check if blank/X/O
+  Piece temp = board[row][column];
+
+  if(temp == X)
+    return X;
+  else if(temp == O)
+    return O;
+  else
     return Blank;
 }
 
@@ -65,5 +136,69 @@ Piece Piezas::pieceAt(int row, int column)
 **/
 Piece Piezas::gameState()
 {
+  //temporarily keep track of X and O totals
+  int xTot = 0;
+  int oTot = 0;
+
+  //tmp track count of X/O horizontally
+  int xHor = 0;
+  int oHor = 0;
+
+  //tmp track count of X/O vertically
+  int xVer = 0;
+  int oVer = 0;
+
+  //if board is empty, game not over
+  if(board.empty())
+    return Invalid;
+
+
+  //check the columns first for counts
+  for(int i = 0; i < BOARD_ROWS; i++)
+  {
+    for(int j = 0; j < BOARD_COLS; j++)
+    {
+      //check if any spaces have blanks or invalids, if so game can't be over
+      if( (board[i][j] == Blank)  )
+        return Invalid;
+
+      if(board[i][j] == X)
+        xVer++;
+      else if(board[i][j] == O)
+        oVer++;
+
+    }
+
+    if(xTot < xVer)
+      xTot = xVer;
+    if(oTot < oVer)
+      oTot = oVer;
+  }
+
+  for(int i = 0; i < BOARD_COLS; i++)
+  {
+    for(int j = 0; j < BOARD_ROWS; j++)
+    {
+      //No need to check for blank space here, already checked in above loop
+
+      if(board[j][i] == X)
+        xHor++;
+      else if(board[j][i] == O)
+        oHor++;
+
+    }
+
+    if(xTot < xHor)
+      xTot = xHor;
+    if(oTot < oHor)
+      oTot = oVer;
+  }
+
+  if(xTot > oTot)
+    return X;
+  else if(oTot > xTot)
+    return O;
+  else
     return Blank;
+
 }
--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -18,3 +18,98 @@ TEST(PiezasTest, sanityCheck)
 {
 	ASSERT_TRUE(true);
 }
+
+//check X's first turn
+TEST(PiezasTest, X_first_turn)
+{
+  Piezas bb = Piezas();
+  Piece X_ret = bb.dropPiece(0);
+  ASSERT_EQ(X_ret, X);
+}
+
+//Turn switched to O, check O's drop
+TEST(PiezasTest, O_first_turn)
+{
+  Piezas bb = Piezas();
+  //Need to drop a piece first to switch turns
+  bb.dropPiece(0);
+  Piece O_ret = bb.dropPiece(0);
+  ASSERT_EQ(O_ret, O);
+}
+
+//Check to see if Piece will drop when column is full
+TEST(PiezasTest, column_full)
+{
+  Piezas bb = Piezas();
+  bb.dropPiece(0);
+  bb.dropPiece(0);
+  bb.dropPiece(0);
+  bb.dropPiece(0);
+
+  Piece too_full = bb.dropPiece(0);
+  ASSERT_EQ(too_full, Blank);
+}
+
+//Test for column out of bounds
+TEST(PiezasTest, column_out_of_bound)
+{
+  Piezas bb = Piezas();
+  Piece out_of_bound = bb.dropPiece(5);
+  ASSERT_EQ(out_of_bound, Invalid);
+}
+
+//Check to see if reset works
+TEST(PiezasTest, resetCheck)
+{
+  Piezas bb = Piezas();
+  bb.dropPiece(0);
+  bb.dropPiece(0);
+  bb.dropPiece(0);
+  bb.dropPiece(0);
+
+  bb.dropPiece(1);
+  bb.dropPiece(1);
+  bb.dropPiece(1);
+  bb.dropPiece(1);
+
+  bb.dropPiece(2);
+  bb.dropPiece(2);
+  bb.dropPiece(2);
+  bb.dropPiece(2);
+
+  bb.dropPiece(3);
+  bb.dropPiece(3);
+  bb.dropPiece(3);
+  bb.dropPiece(3);
+
+  bb.reset();
+
+  ASSERT_EQ(bb.pieceAt(0,0),Blank);
+  ASSERT_EQ(bb.pieceAt(0,1),Blank);
+  ASSERT_EQ(bb.pieceAt(0,2),Blank);
+
+}
+
+//check for winner, X in this case will be winner
+TEST(PiezasTest, X_win)
+{
+  Piezas bb = Piezas();
+
+  bb.dropPiece(0); //X
+  bb.dropPiece(1); //O
+  bb.dropPiece(0); //X
+  bb.dropPiece(2); //O
+  bb.dropPiece(0); //X
+  bb.dropPiece(3); //O
+  bb.dropPiece(1); //X
+  bb.dropPiece(1); //O
+  bb.dropPiece(2); //X
+  bb.dropPiece(2); //O
+  bb.dropPiece(3); //X
+  bb.dropPiece(3); //O
+
+  Piece state = bb.gameState();
+  ASSERT_EQ(state,X);
+
+
+}
